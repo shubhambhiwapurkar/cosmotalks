bcae6e5739f3d36cbc7e6c23b8a078b8e389a2af
shubham <shubhambhiwapurkar@gmail.com>
2025-08-20 17:10:00 +0530

    fix: Skip dev dependencies in build

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-// require('./services/scheduler');
+require('./services/scheduler');
 });
