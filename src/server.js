1085796f4d9f8ff6556dfea68a7871e222283d62
shubham <shubhambhiwapurkar@gmail.com>
2025-08-29 00:20:42 +0530

    yes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 require('./services/scheduler');
 });
